83c3fa70f417d03ced447a0990a1cc2eded11ebb
Mayukh Bagchi <bagchimayukh@yahoo.com>
2022-08-16 20:24:01 +0530

    Time: 78 ms (19.46%), Space: 18.2 MB (16.19%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,37 +1,59 @@
 class Solution {
 public:
     
-    void DFS(vector<vector<char>>& grid, int i, int j){
+    struct node{
+        int x,y;
         
-        if(i<0 || i>=grid.size() || j>=grid[0].size() || j<0 || grid[i][j]=='0' )return;
-        
-        grid[i][j]='0';
-        
-        DFS(grid,i+1,j);
-        DFS(grid,i-1,j);
-        DFS(grid,i,j+1);
-        DFS(grid,i,j-1);
-        
-        
-    }
+        node(int _x, int _y ){
+            x=_x;
+            y=_y;
+        }
+    };
     
-    int numIslands(vector<vector<char>>& grid) {
+    void solve(int I, int J, vector<vector<char>> &grid){
         
-        int n = grid.size();
-        int m = grid[0].size();
+        queue<node>q;
+        grid[I][J]='2';
+        q.push(node(I,J));
         
-        int noOfIsland =0 ;
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j] == '1'){
-                    noOfIsland++;
-                    DFS(grid,i,j);
+        while(!q.empty()){
+            int x = q.front().x;
+            int y = q.front().y;
+            q.pop();
+            int dx[4]= {1,-1,0,0};
+            int dy[4]= {0,0,1,-1};
+            
+            for(int i=0;i<4;i++){
+                int newx = dx[i]+x;
+                int newy =dy[i]+y;
+                
+                if(newx>=0 and newx<grid.size() and newy >=0 and newy < grid[0].size() and grid[newx][newy]=='1'){
+                    q.push(node(newx,newy));
+                    grid[newx][newy]='2';
                 }
             }
         }
+    }
+    
+    
+    
+    int numIslands(vector<vector<char>>& grid) {
         
+     queue<node>q;
         
-        return noOfIsland;
+     int n = grid.size();
+     int m = grid[0].size();
+     int count =0;
+        
+     for(int i=0;i<n;i++){
+         for(int j=0;j<m;j++){
+             if(grid[i][j] == '1'){
+                solve(i,j,grid);
+                 count++;
+             }
+         }
+     }
+        
+        return count;
     }
 };
