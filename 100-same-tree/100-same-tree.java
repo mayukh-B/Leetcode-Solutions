232a61534eb8c2c66c43cc7bea99753838eab90e
Mayukh Bagchi <bagchimayukh@yahoo.com>
2022-09-06 21:03:57 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (6.73%) - LeetHub

--- 100-same-tree/100-same-tree.java
+++ 100-same-tree/100-same-tree.java
@@ -1,23 +1,21 @@
 /**
  * Definition for a binary tree node.
- * public class TreeNode {
+ * struct TreeNode {
  *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
 class Solution {
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p==null || q==null)return (p==q);
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
         
-        if(p.val == q.val && isSameTree(p.right,q.right) && isSameTree(p.left,q.left))return true;
+        if(!p or !q)return (p==q);
+        
+        if(p->val==q->val and isSameTree(p->left,q->left) and isSameTree(p->right,q->right))return true;
         return false;
     }
-}
+};
