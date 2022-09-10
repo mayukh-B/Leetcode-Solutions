6fb4d1c3cda793234153572807513faa4d782737
Mayukh Bagchi <bagchimayukh@yahoo.com>
2022-09-10 21:16:27 +0530

    Time: 15 ms (81.64%), Space: 20.9 MB (80.93%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -13,28 +13,21 @@ class Solution {
     
 private:
     int depth(TreeNode* root){
+        
         if(!root)return 0;
         
         int lh = depth(root->left);
+        if(lh == -1)return -1;
         int rh = depth(root->right);
+        if(rh == -1)return -1;
         
-        return 1+max(lh,rh);
+        if(abs(lh-rh)>1)return -1;
+        
+        return max(lh,rh)+1;
+
     }
 public:
     bool isBalanced(TreeNode* root) {
-         if(!root)return true;
-        int height  = depth(root);
-       
-        int lh = depth(root->left);
-        int rh = depth(root->right);
-        
-        if(abs(lh-rh)>1)return false;
-        
-        bool lt = isBalanced(root->left);
-        bool rt = isBalanced(root->right);
-        
-        if(!lt or !rt)return false;
-        
-        return true;
+            return depth(root)!=-1;
     }
 };
